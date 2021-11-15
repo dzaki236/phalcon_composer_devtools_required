339b4e7f29279e3faa9122f4e98e9db5d94cb642
dzaki236 <dzakiahnafz@gmail.com>
2021-11-15 10:26:10 +0700

    add new incubator

--- src/index.php
+++ src/index.php
@@ -1,9 +1,11 @@
 <?php
 
+use Fabfuel\Prophiler\Plugin\Manager\Phalcon;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Loader;
 use Phalcon\Mvc\Application;
 use Phalcon\Mvc\View;
+// use ;
 
 try {
     // Autoloader
@@ -19,6 +21,9 @@ try {
     $di->set('view', function() {
         $view = new View();
         $view->setViewsDir('../app/views');
+        $view->registerEngines([
+            '.volt'=>'Phalcon\Mvc\View\Engine\Volt'
+        ]);
         return $view;
     });
 
@@ -28,4 +33,5 @@ try {
 
 } catch(\Phalcon\Exception $e) {
     echo $e->getMessage();
+    echo "<br> Try Documentation <a href='https://docs.phalcon.io/4.0/en/introduction'>Here</a>";
 }
